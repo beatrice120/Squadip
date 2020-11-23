060dbfa91f18399a1000afdc7513d81297369cee
beatrice120 <beatrice120>
2020-11-23 17:58:54 +0200

    commenting

--- src/test/java/models/HeroTest.java
+++ src/test/java/models/HeroTest.java
@@ -14,43 +14,37 @@ public class HeroTest {
     @Test
     public void newHero_getName_String() {
         Hero newHero = Hero.setUpNewHero();
-        assertEquals("Atemba",newHero.getName());
+        assertEquals("betty",newHero.getName());// testing if it will be accepting the name in adding hero name
     }
     @Test
     public void newHero_getAge_Int() {
-        Hero newHero = Hero.setUpNewHero();
-        assertEquals(23,newHero.getAge());
+        Hero newHero = Hero.setUpNewHero();// testing if it will be accepting the age
+        assertEquals(30,newHero.getAge());
     }
     @Test
     public void newHero_getPower_String() {
         Hero newHero = Hero.setUpNewHero();
-        assertEquals("flying",newHero.getPower());
+        assertEquals("heat",newHero.getPower());// testing if it will be accepting to enter the hero power
     }
     @Test
     public void newHero_getWeakness_String() {
         Hero newHero = Hero.setUpNewHero();
-        assertEquals("fire",newHero.getWeakness());
+        assertEquals("fire",newHero.getWeakness());//testing if it will accept to enter weakness of a hero
     }
     @Test
     public void newHero_getAllInstances_true() {
         Hero newHero = Hero.setUpNewHero();
         Hero another = Hero.setUpNewHero();
-        assertTrue(Hero.getAllInstances().contains(newHero));
+        assertTrue(Hero.getAllInstances().contains(newHero));// to test if it will be giving us all instances we insearted
         assertTrue(Hero.getAllInstances().contains(another));
     }
-    @Test
-    public void newHero_getId_Int() {
-        Hero.clearAllHeroes();
-        Hero newHero = Hero.setUpNewHero();
-        Hero another = Hero.setUpNewHero();
-        Hero another1 = Hero.setUpNewHero();
-        assertEquals(3,another1.getId());
-    }
+
+
     @Test
     public void newHero_findById_id() {
         Hero.clearAllHeroes();
-        Hero newHero = Hero.setUpNewHero();
+        Hero newHero = Hero.setUpNewHero();// testing if it will be getting the hero ID
         Hero another = Hero.setUpNewHero();
-        assertEquals(2,Hero.findById(another.getId()).getId());
+        assertEquals(1,Hero.findById(another.getId()).getId());
     }
 }
